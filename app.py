122eb8eb403b3b6f7c6b8e6138c6ec0e8a8b9c1b
OzanSaridas09 <ozan_saridas@outlook.com>
2026-01-21 17:37:50 -0500

    Added bootstrap templates for index.html, favorites.html, history.html, user guide (modal), and preferences (modal)

--- app.py
+++ app.py
@@ -2,12 +2,21 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
 def index():
-    return "<h1>Hello, Flask Web Application!</h1><p>The server is running.</p>"
+    return render_template("index.html")
 
-@app.route('/about')
-def about():
-    return render_template("about.html")
+@app.route('/addname')
+def addname():
+    return render_template("addname.html")
+
+@app.route('/favorites')
+def favorites():
+    return render_template("favorites.html")
+
+@app.route('/history')
+def history():
+    return render_template("history.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
